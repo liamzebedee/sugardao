e7a32496eed0c450ba3ac4c5a8ce6da54965b383
liamzebedee <liamzebedee@yahoo.com.au>
2021-06-26 16:44:45 +1000

    rework config for ovm testnet

--- dapp/lib/index.js
+++ dapp/lib/index.js
@@ -1,22 +1,33 @@
-const optimismNetworkConfig = {
-    chainId: '0x1a4',
-    chainName: 'Optimism Local 2',
-    // rpcUrls: ['http://ec2-3-26-43-36.ap-southeast-2.compute.amazonaws.com:8545'],
-    rpcUrls: ['https://sugardao:3001/api/ovm-testnet'],
-    // rpcUrls: ['https://sugardao.vercel.app/api/ovm-testnet'],
-    blockExplorerUrls: ['https://local-explorer.optimism.io/'],
-    iconUrls: [
-        'https://optimism.io/images/metamask_icon.svg',
-        'https://optimism.io/images/metamask_icon.png',
-    ],
-}
+
+const optimismNetworkConfigs = [
+    {
+        chainId: '0x1a4',
+        chainName: 'Optimism Testnet',
+        rpcUrls: ['https://sugardao.vercel.app/api/ovm-testnet'],
+        blockExplorerUrls: ['https://local-explorer.optimism.io/'],
+        iconUrls: [
+            'https://optimism.io/images/metamask_icon.svg',
+            'https://optimism.io/images/metamask_icon.png',
+        ],
+    },
+    {
+        chainId: '0x1a4',
+        chainName: 'Optimism Local',
+        rpcUrls: ['https://sugardao:3001/api/ovm-testnet'],
+        blockExplorerUrls: ['https://local-explorer.optimism.io/'],
+        iconUrls: [
+            'https://optimism.io/images/metamask_icon.svg',
+            'https://optimism.io/images/metamask_icon.png',
+        ],
+    }
+]
 
 const addOptimismNetworkToMetamask = async ({ ethereum }) => {
     if (!ethereum || !ethereum.isMetaMask) throw new Error('Metamask is not installed');
     // const optimismNetworkConfig = getOptimismNetwork({ layerOneNetworkId: Number(ethereum.chainId) });
     return await ethereum.request({
         method: 'wallet_addEthereumChain',
-        params: [optimismNetworkConfig],
+        params: [optimismNetworkConfig[0]],
     });
 }
 
