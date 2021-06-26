8b6ab8302c425f85c1ee4607314571cf99f6ef6e
liamzebedee <liamzebedee@yahoo.com.au>
2021-06-26 16:54:54 +1000

    fix 2

--- README.md
+++ README.md
@@ -21,3 +21,10 @@ npx hardhat run scripts/migrations/1_deploy_loans.ts
 # Sugar feed bot.
 ts-node sugarbot/index.ts node --provider-url http://localhost:8545 --nightscout-url https://EXAMPLE
 ```
+
+
+#### Local OVM testnet proxy
+
+```
+ETH_RPC_URL=https://sugardao.vercel.app/api/ovm-testnet seth block latest
+```
--- dapp/lib/index.js
+++ dapp/lib/index.js
@@ -1,6 +1,6 @@
 
-const optimismNetworkConfigs = [
-    {
+const optimismNetworkConfigs = {
+    test: {
         chainId: '0x1a4',
         chainName: 'Optimism Testnet',
         rpcUrls: ['https://sugardao.vercel.app/api/ovm-testnet'],
@@ -10,7 +10,7 @@ const optimismNetworkConfigs = [
             'https://optimism.io/images/metamask_icon.png',
         ],
     },
-    {
+    local: {
         chainId: '0x1a4',
         chainName: 'Optimism Local',
         rpcUrls: ['https://sugardao:3001/api/ovm-testnet'],
@@ -20,14 +20,13 @@ const optimismNetworkConfigs = [
             'https://optimism.io/images/metamask_icon.png',
         ],
     }
-]
+}
 
 const addOptimismNetworkToMetamask = async ({ ethereum }) => {
     if (!ethereum || !ethereum.isMetaMask) throw new Error('Metamask is not installed');
-    // const optimismNetworkConfig = getOptimismNetwork({ layerOneNetworkId: Number(ethereum.chainId) });
     return await ethereum.request({
         method: 'wallet_addEthereumChain',
-        params: [optimismNetworkConfigs[0]],
+        params: [optimismNetworkConfigs.test],
     });
 }
 
