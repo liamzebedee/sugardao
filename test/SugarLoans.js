94e9fd4366bd90ae3ce5524717923fd39dced7a3
jmcph4 <jmcph4.github@gmail.com>
2021-06-28 22:46:41 +1000

    Add (failing) test for SugarLoans.score

--- test/SugarLoans.js
+++ test/SugarLoans.js
@@ -26,7 +26,36 @@ describe("Unit tests: Trader.sol", async () => {
     context("When called", async () => {
       it("Reverts", async () => {
         const amount = ethers.constants.One;
-        await expect(SugarLoans.openLoan(amount)).to.be.revertedWith("");
+        expect(SugarLoans.openLoan(amount)).to.be.revertedWith("");
+      });
+    });
+  });
+
+  describe("score", async () => {
+    context("When called with BGL above target", async () => {
+      it("Returns the correct score", async () => {
+        const one = ethers.constants.One.mul(ethers.constants.WeiPerEther);
+        const eleven = ethers.BigNumber.from("11").mul(
+          ethers.constants.WeiPerEther
+        );
+        const seven = ethers.BigNumber.from("7").mul(
+          ethers.constants.WeiPerEther
+        );
+        const eighteen = ethers.BigNumber.from("18").mul(
+          ethers.constants.WeiPerEther
+        );
+        const targetBGL = seven;
+        const upperBGLBound = eighteen;
+
+        const actualBGL = ethers.BigNumber.from("8").mul(
+          ethers.constants.WeiPerEther
+        );
+
+        const fValue = actualBGL.sub(targetBGL).div(upperBGLBound);
+
+        const expectedScore = one.sub(fValue);
+
+        expect(await SugarLoans.score(actualBGL)).to.be.equal(expectedScore);
       });
     });
   });
